5d924f1f7112a306def12072670cb769c31d6f42
soren <soren.benson2@gmail.com>
2025-09-11 10:35:13 -0500

    working on collisions

---
+++ Collisions.java
@@ -0,0 +1,9 @@
+public class Collisions {
+    public static boolean samePosition(RandomWalk p1, RandomWalk p2){
+        if (p1.getX() == p2.getX() && p1.getY() == p2.getY()){
+            
+        }
+
+    }
+}
+
